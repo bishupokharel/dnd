5d501804c3d18b51f901aaa73a274173b092a222
bishupokharel@gmail.com <bishupokharel@gmail.com>
2013-05-30 12:11:27 -0400

    first branch commit

--- dnd/src/np/com/dnd/HelloWorld.java
+++ dnd/src/np/com/dnd/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World 123");
+		System.out.println("Hello world from local branch");
 
 	}
 
